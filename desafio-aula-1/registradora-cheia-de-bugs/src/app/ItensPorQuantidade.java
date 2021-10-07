40f924f00b7c00b395c7c7cbeda8672a4d7c45af
Carlos Leuck <carlinhosrlz@gmail.com>
2021-10-06 23:20:19 -0300

    Criado métodos para remover os produtos do estoque

--- desafio-aula-1/registradora-cheia-de-bugs/src/app/ItensPorQuantidade.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/app/ItensPorQuantidade.java
@@ -3,10 +3,36 @@ package app;
 public class ItensPorQuantidade {
 
     static int pao = 3600;
-    static int torta = 4;
+    static int torta = 64;
     static int sanduiche = 20;
     static int leite = 20;
     static int cafe = 20;
+
+    public static void removerCafe(int quantidade) {
+        cafe -= quantidade;
+
+    }
+
+    public static void removerLeite(int quantidade) {
+        leite -= quantidade;
+
+    }
+
+    public static void removerPao(int quantidade) {
+        pao -= quantidade*60;
+
+    }
+
+    public static void removerSanduiche(int quantidade) {
+        sanduiche -= quantidade;
+
+    }
+
+    public static void removerTorta(int quantidade) {
+        torta -= quantidade;
+
+    }
+
 
 
 }
