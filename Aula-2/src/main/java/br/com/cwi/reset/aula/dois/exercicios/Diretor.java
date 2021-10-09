dd21fd7f5f0567948fc9d1af2967a4201fa7550c
Carlos Leuck <carlinhosrlz@gmail.com>
2021-10-09 11:25:02 -0300

    Classe Diretor atualizada

--- Aula-2/src/main/java/br/com/cwi/reset/aula/dois/exercicios/Diretor.java
+++ Aula-2/src/main/java/br/com/cwi/reset/aula/dois/exercicios/Diretor.java
@@ -2,10 +2,10 @@ package br.com.cwi.reset.aula.dois.exercicios;
 
 public class Diretor {
     private String nome;
-    private int idade;
-    private int quantidadeFilmesDirigidos;
+    private Integer idade;
+    private Integer quantidadeFilmesDirigidos;
 
-    public Diretor(String nome, int idade, int quantidadeFilmesDirigidos) {
+    public Diretor(String nome, Integer idade, Integer quantidadeFilmesDirigidos) {
         this.nome = nome;
         this.idade = idade;
         this.quantidadeFilmesDirigidos = quantidadeFilmesDirigidos;
