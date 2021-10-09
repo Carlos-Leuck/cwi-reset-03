ffb5c31178547a0e1a2b4db1ad5c8e256ad3d17c
Carlos Leuck <carlinhosrlz@gmail.com>
2021-10-09 10:41:32 -0300

    "Primeiro exercicio da aula 2: resolvido"

---
+++ Aula-2/src/main/java/br/com/cwi/reset/aula/dois/Aplicacao.java
@@ -0,0 +1,14 @@
+package br.com.cwi.reset.aula.dois;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Diretor diretor1 = new Diretor("Espen Sandberg", 49, 10);
+        Diretor diretor2 = new Diretor("Spilber", 55, 23);
+        Filme filme1 = new Filme("Piratas do Caribe", "filme de ação muito top", 90, 2003, 4, diretor1);
+        Filme filme2 = new Filme("Monstros S.A", "animação muito legal", 120, 2009, 5, diretor2);
+
+        System.out.println(filme1.reproduzir());
+        System.out.println(filme2.reproduzir());
+    }
+
+}
