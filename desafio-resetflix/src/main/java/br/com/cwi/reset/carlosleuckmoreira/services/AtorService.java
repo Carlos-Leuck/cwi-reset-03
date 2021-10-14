15278ffab4628708483fd9106cd8853359dedfdb
Carlos Leuck <carlinhosrlz@gmail.com>
2021-10-14 09:11:01 -0300

    AtorService: mensagens de erro validadas

--- desafio-resetflix/src/main/java/br/com/cwi/reset/carlosleuckmoreira/services/AtorService.java
+++ desafio-resetflix/src/main/java/br/com/cwi/reset/carlosleuckmoreira/services/AtorService.java
@@ -1,5 +1,6 @@
 package br.com.cwi.reset.carlosleuckmoreira.services;
 
+import br.com.cwi.reset.carlosleuckmoreira.StatusCarreira;
 import br.com.cwi.reset.carlosleuckmoreira.dto.AtorEmAtividade;
 import br.com.cwi.reset.carlosleuckmoreira.dto.AtorRequest;
 import br.com.cwi.reset.carlosleuckmoreira.repositories.FakeDatabase;
@@ -18,51 +19,57 @@ public class AtorService {
     public AtorService(FakeDatabase fakeDatabase) {
         this.fakeDatabase = fakeDatabase;
     }
-    //TODO ID DEVE SER INCREMENTAL E ÚNICO: falta implementar e testar.
 
     public void criarAtor(AtorRequest atorRequest) {
 
-        //TODO validação campo obrigatório: falta retornar o campo e testar.
+        //validação campo obrigatório ator: OK
         try {
-            if (atorRequest.getNome() == null || atorRequest.getDataNascimento() == null
-                    || atorRequest.getAnoInicioAtividade() == null || atorRequest.getStatusCarreira() == null) {
-                throw new CampoObrigatorioNaoInformadoException();
+            if (atorRequest.getNome() == null) {
+                throw new CampoObrigatorioNaoInformadoException("nome");
+            }
+            if (atorRequest.getDataNascimento() == null) {
+                throw new CampoObrigatorioNaoInformadoException("data de nascimento");
+            }
+            if (atorRequest.getAnoInicioAtividade() == null) {
+                throw new CampoObrigatorioNaoInformadoException("ano de inicio da atividade");
+            }
+            if (atorRequest.getStatusCarreira() == null) {
+                throw new CampoObrigatorioNaoInformadoException("status da carreira");
             }
 
-            //TODO validação nome e sobrenome: falta testar.
+
+            //validação nome e sobrenome: OK
             if (!atorRequest.getNome().contains(" ")) {
                 throw new NomeESobrenomeDevemSerInformadosException();
             }
 
-            //TODO validação ano de início de atividade não pode ser anterior ao ano de nascimento do ator: falta testar
             LocalDate nascimento = atorRequest.getDataNascimento();
             Integer inicioAtividade = atorRequest.getAnoInicioAtividade();
-//            LocalDate dataInteiroToLocalDate = LocalDate.parse(inicioAtividade.toString(), DateTimeFormatter.BASIC_ISO_DATE);
 
             SimpleDateFormat newFormat = new SimpleDateFormat("yyyy");
             String dataInteiroToLocalDate = newFormat.format(inicioAtividade);
 
-
-            int compararDatas = dataInteiroToLocalDate.compareTo(String.valueOf(nascimento));
-            //Se der valor positivo então inicioAtividade > data de nascimento
-            if (!(compararDatas >0)) {
-                throw new AnoDeInicioDeAtividadeDeveSerMaiorQueNascimentoDoAtorException();
-            }
-
-            // TODO validação ator não nascido: falta testar
-            //validação não é possível cadastrar atores não nascidos
-            //se der positivo a data de nascimento > data atual
-            //Se der 0 significa que data de nascimento == data atual
-            if (nascimento.compareTo(LocalDate.now()) >= 0) {
+            /*
+            validação não é possível cadastrar atores não nascidos:OK
+            Obs: se der positivo a data de nascimento > data atual
+            */
+            if (nascimento.compareTo(LocalDate.now()) > 0) {
                 throw new DataDeNascimentoInvalidaException();
             }
 
-            //TODO validação já existe um ator cadastrado com esse nome: falta testar
+            //validação ano de início de atividade não pode ser anterior ao ano de nascimento do ator: OK
+            int compararDatas = dataInteiroToLocalDate.compareTo(String.valueOf(nascimento));
+            if (compararDatas <= 0) {
+                throw new AnoDeInicioDeAtividadeDeveSerMaiorQueNascimentoDoAtorException();
+            }
+
+
+            //validação já existe um ator cadastrado com esse nome: OK
             String nomeAtor;
             nomeAtor = atorRequest.getNome();
             for (int i = 0; i < fakeDatabase.recuperaAtores().size(); i++) {
                 if (fakeDatabase.recuperaAtores().get(i).getNome().equals(nomeAtor)) {
-                    throw new AtorJaCadastradoException();
+                    throw new AtorJaCadastradoException(nomeAtor);
                 }
             }
 
@@ -81,30 +88,28 @@ public class AtorService {
     public List listarAtoresEmAtividade(String filtroNome) {
         List<Ator> lista = new ArrayList();
         lista = fakeDatabase.recuperaAtores();
-        // TODO validação Caso não exista atores cadastrados deve retornar a mensagem: falta testar
+
         try {
             if (lista.isEmpty()) {
                 throw new NaoExisteAtorCadastradoException();
             }
 
-
             for (int i = 0; i < lista.size(); i++) {
-                if (!lista.get(i).getStatusCarreira().toString().equals(filtroNome)) {
+                if (lista.get(i).getStatusCarreira() == StatusCarreira.APOSENTADO) {
                     lista.remove(i);
-
                 }
             }
 
-            //TODO  validação Caso não seja encontrado nenhum ator com o filtro deve retornar a mensagem: falta testar
-            if (lista.isEmpty()) {
-                throw new NaoExisteAtorComOFiltroInformadoException();
+            for (int i = 0; i < lista.size(); i++) {
+                if (!lista.get(i).getNome().contains(filtroNome) && lista.isEmpty()) {
+                    throw new NaoExisteAtorComOFiltroInformadoException();
+                }
             }
 
         } catch (NaoExisteAtorCadastradoException | NaoExisteAtorComOFiltroInformadoException e) {
             e.printStackTrace();
         }
-
-        return lista.stream().map(x -> new AtorEmAtividade(x)).collect(Collectors.toList());
+        return lista.stream().map(AtorEmAtividade::new).collect(Collectors.toList());
 
     }
 
@@ -112,27 +117,27 @@ public class AtorService {
         List<Ator> lista = new ArrayList();
         lista = fakeDatabase.recuperaAtores();
 
-        //TODO filtro ID é obrigatório: falta testar
         try {
             if (id == null) {
-                throw new CampoObrigatorioNaoInformadoException();
+                throw new CampoObrigatorioNaoInformadoException("ID");
             }
-            //TODO Caso não encontrado o Ator: falta testar
 
             for (int i = 0; i < lista.size(); i++) {
-                if (!lista.get(i).getId().equals(id)) {
-                    throw new NaoExisteAtorComOIdInformadoException();
-                } else return lista.get(i);
+                if ((lista.get(i).getId()) == (id)) {
+                    return lista.get(i);
+
+                }
             }
+            throw new NaoExisteAtorComOIdInformadoException(id);
+
         } catch (CampoObrigatorioNaoInformadoException | NaoExisteAtorComOIdInformadoException e) {
             e.printStackTrace();
         }
-        return lista.get(id);
+        return null;
 
     }
 
     public List consultarAtores() {
-        //TODO validação Caso não exista atores cadastrados deve retornar a mensagem: falta testar
         try {
             if (fakeDatabase.recuperaAtores().isEmpty()) {
                 throw new NaoExisteAtorCadastradoException();
