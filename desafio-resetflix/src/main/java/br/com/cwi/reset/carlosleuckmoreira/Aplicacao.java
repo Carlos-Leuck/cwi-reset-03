371c401e46ec34308480ad0651924b3c8ed471f3
Carlos Leuck <carlinhosrlz@gmail.com>
2021-10-14 05:14:25 -0300

    testes na classe da Aplicacao: consultarAtor e consultarAtores

--- desafio-resetflix/src/main/java/br/com/cwi/reset/carlosleuckmoreira/Aplicacao.java
+++ desafio-resetflix/src/main/java/br/com/cwi/reset/carlosleuckmoreira/Aplicacao.java
@@ -27,17 +27,37 @@ public class Aplicacao {
         Integer anoInicioAtividade2 = 1986;
         AtorRequest atorRequest2 = new AtorRequest(nome2, dataNascimento2, statusCarreira2, anoInicioAtividade2);
 
+        String nome3 = "Tom Cruise";
+        LocalDate dataNascimento3 = LocalDate.of(1968, Month.SEPTEMBER, 25);
+        StatusCarreira statusCarreira3 = StatusCarreira.EM_ATIVIDADE;
+        Integer anoInicioAtividade3 = 1986;
+        AtorRequest atorRequest3 = new AtorRequest(nome3, dataNascimento3, statusCarreira3, anoInicioAtividade3);
+
         atorService.criarAtor(atorRequest);
         atorService.criarAtor(atorRequest2);
-
-//       atorService.consultarAtor(2);
-
-
-        List<Ator> atores = fakeDatabase.recuperaAtores();
+        atorService.criarAtor(atorRequest3);
 
 
-        System.out.println("Deve conter 1 ator, quantidade encontrada: " + atores.size());
-        System.out.println("Id:" + atores.get(0).getId());
-        System.out.println("Primeiro ator deve ser 'Will Smith', valor encontrado: " + atores.get(0).getNome());
+//        List<Ator> atores = fakeDatabase.recuperaAtores();
+
+
+//        System.out.println("Deve conter 1 ator, quantidade encontrada: " + atores.size());
+//        System.out.println("Id:" + atores.get(0).getId());
+//        System.out.println("Primeiro ator deve ser 'Will Smith', valor encontrado: " + atores.get(0).getNome());
+//        System.out.println(atorService.consultarAtores().toString());
+
+
+
+        // TESTE CONSULTAR ATOR POR ID: FUNCIONANDO, ARRUMAR MSG EXCEÇÃO
+        //System.out.println("CONSULTAR ATORES POR ID");
+        //System.out.println(atorService.consultarAtor(null));
+        //System.out.println(atorService.consultarAtor(0));
+        //System.out.println(atorService.consultarAtor(1));
+
+
+
+        //TESTE CONSULTAR TODOS OS ATORES: OK.
+//       System.out.println("CONSULTAR TODOS ATORES");
+//       System.out.println(atorService.consultarAtores());
     }
 }
